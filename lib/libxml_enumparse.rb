b9b3293f318fedd621e1760a214c3dc60cb44c03
constdrop <iota_tz@yahoo.co.jp>
2023-12-12 21:26:37 +0900

    first commit

---
+++ lib/libxml_enumparse.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "libxml_enumparse/version"
+
+module LibxmlEnumparse
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/libxml_enumparse_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LibxmlEnumparse do
+  it "has a version number" do
+    expect(LibxmlEnumparse::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
